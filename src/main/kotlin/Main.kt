e050fae4af5f1cd31dcadf5e6e5a8f60e065a3f6
lucas.albuquerque <lucas.albuquerque@m4u.com.br>
2017-02-14 18:31:24 -0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+import java.security.InvalidParameterException
+
+fun calculateWithdraw() {
+   throw InvalidParameterException("Invalid amount.")
+}
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,12 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+
+class MainTest {
+
+    @Test
+    fun sumWorks() {
+        assertEquals(1 + 1, 2)
+    }
+
+}
