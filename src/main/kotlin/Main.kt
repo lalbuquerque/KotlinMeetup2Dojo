a8a396e1a8424c320748ea119909caf6e4d22429
rbispo <rbispo21@gmail.com>
2017-02-16 14:26:40 -0200

    add dojo solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,34 @@
-import java.security.InvalidParameterException
+fun calculateWithdraw(input: Int): List<Int> {
+    val bills = listOf(100, 50, 20, 10, 5, 2)
 
-fun calculateWithdraw() {
-   throw InvalidParameterException("Invalid amount.")
+    if (input <= 0)
+        throw IllegalArgumentException("Invalid Value")
+
+    if (bills.contains(input))
+        return listOf(input)
+
+    val result = mutableListOf<Int>()
+
+    var inputTmp = input
+
+    bills.forEach {
+
+        val div: Int = inputTmp / it
+        if (div > 0) {
+            for (i in 1..div) {
+                result.add(it)
+                inputTmp -= it
+            }
+        }
+    }
+
+    if (inputTmp > 0)
+        throw IllegalArgumentException("Invalid Value")
+
+    return result
+
+}
+
+fun calculate {
+
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,12 +1,84 @@
+
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
 
 class MainTest {
 
     @Test
-    fun sumWorks() {
-        assertEquals(1 + 1, 2)
+    fun inputAmount100() {
+        val result = calculateWithdraw(100)
+        assertEquals(listOf(100), result)
+    }
+
+    @Test
+    fun inputAmount200() {
+        val result = calculateWithdraw(200)
+        assertEquals(listOf(100, 100), result)
+    }
+
+    @Test
+    fun inputAmount150() {
+        val result = calculateWithdraw(150)
+        assertEquals(listOf(100, 50), result)
+    }
+
+    @Test
+    fun inputAmount10() {
+        val result = calculateWithdraw(10)
+        assertEquals(listOf(10), result)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun inputAmount1() {
+        calculateWithdraw(1)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun inputAmount11() {
+        calculateWithdraw(11)
+    }
+
+    @Test
+    fun inputAmount30() {
+        val result = calculateWithdraw(30)
+        assertEquals(listOf(20, 10), result)
+    }
+
+    @Test
+    fun inputAmount180() {
+        val result = calculateWithdraw(180)
+        assertEquals(listOf(100, 50, 20, 10), result)
+    }
+    @Test
+    fun inputAmount8() {
+        val result = calculateWithdraw(8)
+        assertEquals(listOf(2, 2, 2, 2), result)
+    }
+
+    @Test
+    fun inputAmount4() {
+        val result = calculateWithdraw(4)
+        assertEquals(listOf(2, 2), result)
+    }
+
+    @Test
+    fun inputAmount7() {
+        val result = calculateWithdraw(7)
+        assertEquals(listOf(5, 2), result)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun inputAmount0() {
+        val result = calculateWithdraw(0)
+        assertEquals(emptyList<Int>(), result)
+    }
+
+
+    @Test(expected = IllegalArgumentException::class)
+    fun inputAmount_1() {
+        calculateWithdraw(-1)
     }
 
 }
